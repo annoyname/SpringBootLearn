5cfa168ff571ee555a83cda68411471734edd571
guoqingg_1 <guoqingg_1@tujia.com>
2018-12-10 11:43:43 +0800

    初始化项目

---
+++ src/main/java/org/ggq/springbootlear/SpringBootLearApplication.java
@@ -0,0 +1,12 @@
+package org.ggq.springbootlear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearApplication.class, args);
+    }
+}
---
+++ src/test/java/org/ggq/springbootlear/SpringBootLearApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ggq.springbootlear;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLearApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
